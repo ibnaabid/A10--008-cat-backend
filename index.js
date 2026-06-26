c96e37fe3006505bddc7c00978a04ebb6c75a010
MD Ibna Abid <mdabout2@gmail.com>
2026-06-26 10:50:29 +0600

    update favorites api

--- index.js
+++ index.js
@@ -429,7 +429,7 @@ app.get("/reject-feedback/:id",ownerVerify, async (req, res) => {
       }
     });
 
-    app.get("/favorites",tenantVerify, async (req, res) => {
+    app.get("/favorites", async (req, res) => {
       const result = await favoriteProperty.find().toArray();
       res.send(result);
     });
@@ -457,7 +457,7 @@ app.get("/reject-feedback/:id",ownerVerify, async (req, res) => {
     });
 
 
-await client.db("admin").command({ ping: 1 });
+// await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
   
